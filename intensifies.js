397f84cf44abaed8cdc46e1c044c2746d8128127
pvrs12 <pvrs12@gmail.com>
2018-12-18 01:47:57 -0500

    working

--- intensifies.js
+++ intensifies.js
@@ -1,43 +1,112 @@
-function print(f) {
-    console.log("me: ", f);
-}
-
 function intensify(element, intensifactor) {
     element.classList.add("intensified");
     element.style.setProperty("--intensifactor", intensifactor);
 }
 
-var re = /(\*\*+)([^\*]*)(\1)+/g
+function make_intense(text, intensifactor) {
+    var s = document.createElement("span");
+    s.appendChild(document.createTextNode(text));
+    intensify(s, intensifactor);
+    return s;
+}
 
-function messages_container_listener(mutationsList, observer) {
+function min(a, b){
+    if (a< b){
+        return a;
+    }
+    return b;
+}
+
+function messages_container_listener() {
     var messages = document.getElementsByClassName("c-message__body");
     for(var i=0; i< messages.length; ++i) {
         var message = messages[i];
-        for(var c=0)
-        print(message.textContent);
 
-        var matches = message.textContent.match(re);
-        for(match of matches){
-            
+        var new_children = [];
+        for(var j = 0; j<message.childNodes.length; ++j){
+            if(message.childNodes[j].nodeType == Node.TEXT_NODE) {
+                //intensify the '*'
+                var t = message.childNodes[j].textContent;
+                var intensifies = [];
+                var text = "";
+                var intensifying = false;
+                for(var c = 0; c < t.length -1; c++) {
+                    if(t[c] == '*') {
+                        if (t[c+1] == '*') {
+                            if (!intensifying) {
+                                intensifying = true;
+                                intensifies.push(make_intense(text, 0));
+                                text = "";
+                                var intensifactor_left = 0;
+                                //count remainging '*' for intesnsifactor
+                                for(var d = c; d < t.length; ++d){
+                                    if (t[d]=='*'){
+                                        c++;
+                                        intensifactor_left ++;
+                                    } else {
+                                        break;
+                                    }
+                                }
+                                c--;
+                            } else {
+                                //eat the remaining intensifactor '*'s
+                                var intensifactor_right = 0;
+                                for(var d = c; d < t.length; ++d){
+                                    if (t[d]=='*'){
+                                        c++;
+                                        intensifactor_right++;
+                                    } else {
+                                        break;
+                                    }
+                                }
+                                intensifies.push(make_intense(text, min(intensifactor_left, intensifactor_right)));
+                                text = "";
+                                intensifying = false;
+                                intensifactor = 0;
+                            }
+                        } else {
+                            text += t[c];
+                        }
+                    } else {
+                        text += t[c];
+                    }
+                }
+                //put any remaining text in a node
+                if (t[t.length-1] != '*'){
+                    text += t[t.length-1];
+                }
+                intensifies.push(make_intense(text, 0));
+                for(var k=0; k<intensifies.length; ++k){
+                    new_children.push(intensifies[k]);
+                }
+            } else {
+                new_children.push(message.childNodes[j]);
+            }
         }
-        if(m){
-            print("match!");
-            print(m[0]);
-            print(m[1]);
-            var intensifactor = m[0];
-            var text = m[1];
-
-            var intense = document.createElement("span");
-            intense.appendChild(document.createTextNode(text));
-            intensify(intense, intensifactor);
+        while(message.hasChildNodes()) {
+            message.removeChild(message.lastChild);
+        }
+        for(var child of new_children) {
+            message.appendChild(child);
         }
 
     }
 }
 
+function obs() {
+    //disconnect to prevent loop
+    observer.disconnect();
+    messages_container_listener();
+    //reconnect
+    observer.observe(messages_container, {
+        childList: true,
+        subtree: true
+    });
+}
+
 var messages_container = document.getElementById("messages_container");
 
-var observer = new MutationObserver(messages_container_listener);
+var observer = new MutationObserver(obs);
 observer.observe(messages_container, {
     childList: true,
     subtree: true
